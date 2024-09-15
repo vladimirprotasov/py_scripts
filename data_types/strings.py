988f10e0a3efab46ed521f8593accba88abf8875
Vladimir Protasov <vladimir.protasov@gmail.com>
2024-09-15 12:13:01 +0300

    Updated strings.py

--- data_types/strings.py
+++ data_types/strings.py
@@ -1,12 +1,16 @@
 """String is  immutable sequence - array of characters"""
 
-
 test_string = "This is the test string"
 
-# Counting a certain occurrence of chars
-check: str = chr(116) + chr(104)  # th
-print(test_string.count(check))
+l_strip = test_string.lstrip("This ")  # the test string
+check: str = chr(116) + chr(104)
+counter = test_string.count(check)  # count th
 
+rem_prefix = test_string.removeprefix("This ")  # is the test string
 
-print(test_string.lstrip('This '))  # the test string
-print(test_string.removeprefix('This '))  # is the test string
+data_index = test_string.index("test")
+
+is_alpha_false = test_string.isalpha()  # False
+is_alpha_true = test_string.replace(" ", "").isalpha()  # True
+
+string_list = test_string.split(" ")
